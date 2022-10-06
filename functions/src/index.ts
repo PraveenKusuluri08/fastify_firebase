20645d56f9457bad04a3deb9d5ecdddee6b302b9
PraveenKusuluri08 <praveen.k@skil.ai>
2022-10-06 11:29:30 +0530

    ESLINT-REMOVED

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,7 @@
 import * as functions from "firebase-functions";
 import * as express from "express";
 
-export const helloWorld = functions.https.onRequest(
-    (request:express.Request, response:express.Response) => {
+export const helloWorld = functions.https.onRequest((request:express.Request, response:express.Response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
 });
