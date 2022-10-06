d9f498df7ebbf53a7cbe9ae0ac31cf77e5ca9218
PraveenKusuluri08 <praveen.k@skil.ai>
2022-10-06 10:48:17 +0530

    Express-Added

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,7 @@
 import * as functions from "firebase-functions";
+import * as express from "express"
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-export const helloWorld = functions.https.onRequest((request, response) => {
+export const helloWorld = functions.https.onRequest((request:express.Request, response:express.Response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
 });
