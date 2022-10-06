c6aa73c023a6675d02e14e8f3a5ca74443899a7b
PraveenKusuluri08 <praveen.k@skil.ai>
2022-10-06 11:37:20 +0530

    Function export chnaged

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,7 @@
 import * as functions from "firebase-functions";
 import * as express from "express";
 
-export const helloWorld = functions.https.onRequest((request:express.Request, response:express.Response) => {
+exports.helloWorld = functions.https.onRequest((request:express.Request, response:express.Response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
 });
