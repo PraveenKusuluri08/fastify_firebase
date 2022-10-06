4e6d14d4c8b9b1f704abcb9e604c9a313beba9f8
PraveenKusuluri08 <praveen.k@skil.ai>
2022-10-06 11:25:35 +0530

    Workflow changed

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,8 @@
 import * as functions from "firebase-functions";
 import * as express from "express";
 
-export const helloWorld = functions.https.onRequest((request:express.Request, response:express.Response) => {
+export const helloWorld = functions.https.onRequest(
+    (request:express.Request, response:express.Response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
 });
