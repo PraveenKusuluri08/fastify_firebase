8e9b4131d036be8484e1e233295a59cff033d648
PraveenKusuluri08 <praveen.k@skil.ai>
2022-10-07 14:37:16 +0530

    workflow changed

--- functions/src/services/Users/controllers/handlers/userHandler.ts
+++ functions/src/services/Users/controllers/handlers/userHandler.ts
@@ -7,12 +7,12 @@ export const Test = async (req: any, reply: any) => {
     }).then((doc) => {
         doc.forEach(doc => {
             console.log(doc.data())
-            reply.code(200).send({name: doc.data().name})
+             return reply.code(200).send({name: doc.data().name})
         })
     })
         .catch(err => {
             console.log(err)
-            reply.code(400).send({message: "Failed to get the data"})
+            return reply.code(400).send({message: "Failed to get the data"})
         })
 }
 
